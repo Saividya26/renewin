4daf0afb3510cadb82323e3753451c81049cb9a6
Saividya2001 <saividya.xworkz@gmail.com>
2023-07-31 17:19:33 +0530

    Association

---
+++ Constructors/Browser/src/com/xworkz/browser/app/Internet.java
@@ -0,0 +1,13 @@
+package com.xworkz.browser.app;
+
+public class Internet {
+	
+		static void downloadVideos()
+		{
+			System.out.println("Invoking downloadVideos in Internet..");
+		}
+		static void watchVedios()
+		{
+			System.out.println("Invoking watchVedios in Internet..");
+		}
+}
